15246b89a29700bbc562ee2f5ecf7bd76c3e9d5c
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-09-21 10:21:18 +0200

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Manyuchi, Carrington C on 2024/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
